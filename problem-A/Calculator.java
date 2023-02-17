b43036bec53e738713c16ba3004834a774d0c966
zhaniyazhaniya <124429520+zhaniyazhaniya@users.noreply.github.com>
2023-02-18 04:02:07 +0600

    Update Calculator.java

--- problem-A/Calculator.java
+++ problem-A/Calculator.java
@@ -1,68 +1,65 @@
 public class Calculator {
-    private int x;
-    private int y;
-
-    public Calculator(int x, int y){
+    int x ;
+    int y ;
+    
+    public Calculator( int x , int y){
         this.x = x;
         this.y = y;
     }
-
-    public void setX(int x) {
+    
+    public int getX(){
+        return this.x;
+    }
+    
+    public void setX(int x){
         this.x = x;
+    }
+    
+    public int getY() {
+        return this.y;
     }
 
     public void setY(int y) {
         this.y = y;
     }
 
-    public int getX() {
-        return this.x;
-    }
-
-    public int getY() {
-        return this.y;
-    }
-
-    public int add() throws ArithmeticException{
-        try{
-            if(x < 0 || y<0) throw new ArithmeticException("Enter positive number");
+    public int add(){
+        if (x < 0 || y < 0 ) throw new ArithmeticException("Enter a positive number");
+        try {
             return x + y;
-        }
-        catch(ArithmeticException e){
+        } catch (ArithmeticException e) {
             System.out.println(e.getMessage());
             throw e;
         }
     }
-
-    public int subtract() throws ArithmeticException {
-       if (x < 0 || y < 0 ) throw new ArithmeticException("Enter positive number");
+    
+    public int subtract() throws  ArithmeticException {
+        if (x < 0 || y < 0 ) throw new ArithmeticException("Enter a positive number");
         try {
             return x - y;
-        } catch (ArithmeticException ex) {
+        } catch (ArithmeticException e) {
             System.out.println(e.getMessage());
             return 0;
         }
     }
-
-    public int multiplication() throws ArithmeticException{
-        if( x==0 || y == 0) throw new ArithmeticException("Number cannot be zero");
-        try{
-            return x*y;
-        }
-        catch(ArithmeticException e){
+    
+    public int multiplication() throws  ArithmeticException {
+        if (x == 0 || y == 0 ) throw new ArithmeticException("Number cannot be zero");
+        try {
+            return x * y;
+        } catch (ArithmeticException e) {
             System.out.println(e.getMessage());
             return 0;
         }
     }
-
-    public int division() throws ArithmeticException{
-        if( x==0 || y == 0) throw new ArithmeticException("Number cannot be zero");
-        try{
-            return x/y;
-        }
-        catch(ArithmeticException e){
+    public int division() throws  ArithmeticException {
+        if (x == 0 || y == 0 ) throw new ArithmeticException("Number cannot be zero");
+        try {
+            return x / y;
+        } catch (ArithmeticException e) {
             System.out.println(e.getMessage());
             throw e;
         }
     }
+
 }
